8d5cc0a44f7b423421b9b95eebf760fe97555d64
FreeThought <alex.brough@yahoo.com>
2019-06-22 00:14:01 -0400

    Created GUI placeholders for server ID and peer list.
    Also changed several font sizes in the GUI to percentages.

--- js/gui.js
+++ js/gui.js
@@ -17,7 +17,7 @@ function createGUI(){
 	chattext.resizeToFit = true;
 	chattext.text = "Chat Start:";
 	chattext.color = "white";
-	chattext.fontSize = 14;
+	chattext.fontSize = "14%";
 	chattext.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
 	chattext.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
 	chattext.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
@@ -25,7 +25,7 @@ function createGUI(){
 	
 	chatinput = new BABYLON.GUI.InputText();
 	chatinput.width = "90%";
-	chatinput.fontSize = 14;
+	chatinput.fontSize = "2.5%";
 	chatinput.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
 	chatinput.color = "lightgreen";
 	chatinput.resizeToFit = true;
@@ -50,6 +50,29 @@ function createGUI(){
 		engine.enterFullscreen();
 	});
 	advancedTexture.addControl(button1);    
+	
+	serverIDrect = new BABYLON.GUI.TextBlock();
+	serverIDrect.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+	serverIDrect.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+	serverIDrect.text = "Server ID:\nPlaceholder";
+	serverIDrect.color = "white";
+	serverIDrect.fontSize = "2%";
+	serverIDrect.resizeToFit = true;
+	serverIDrect.paddingRight = "1%";
+	serverIDrect.paddingTop = "1%";
+	advancedTexture.addControl(serverIDrect);
+	
+	peerListRect = new BABYLON.GUI.TextBlock();
+	peerListRect.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+	peerListRect.verticalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_TOP;
+	peerListRect.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+	peerListRect.text = "Peers:\nYou\nMe\nPlaceholder";
+	peerListRect.color = "lightblue";
+	peerListRect.fontSize = "2%";
+	peerListRect.top = "10%";
+	peerListRect.resizeToFit = true;
+	peerListRect.paddingRight = "1%";
+	advancedTexture.addControl(peerListRect);
 }
 
 
